4a4098ea804abebbb7eef71cac9b3756daf3e4fb
shlomo <shlomo2096@gmail.com>
2025-09-14 11:41:39 +0300

    add kafka_consumer

---
+++ event_processor_service/kafka_consumer.py
@@ -0,0 +1,19 @@
+from kafka import KafkaConsumer
+import json
+import config
+
+class KafkaSubscriber:
+    """
+    Subscriber for consuming messages from a Kafka topic.
+    """
+    def __init__(self, topic: str, group_id: str):
+        self.consumer = KafkaConsumer(
+            bootstrap_servers=config.KAFKA_BOOTSTRAP,
+            group_id=group_id,
+            value_deserializer=lambda m: json.loads(m.decode("utf-8"))
+        )
+        self.consumer.subscribe([topic])
+
+    def listen(self):
+        for msg in self.consumer:
+            yield msg.value
