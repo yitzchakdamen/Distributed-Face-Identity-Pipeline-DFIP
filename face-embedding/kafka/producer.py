d7c3af7910cec6ca252e0a5db4c22a35700a2e56
ootb <battles-cyclic-87@icloud.com>
2025-09-14 12:35:50 +0300

    feat: Add configuration settings for Kafka, Elasticsearch, and MongoDB

--- face-embedding/kafka/producer.py
+++ face-embedding/kafka/producer.py
@@ -16,14 +16,23 @@ class KafkaProducer:
         else:
             logger.info(f"Message delivered to {msg.topic()} [{msg.partition()}]")
 
-    def produce(self, topic: str, message: dict):
+    def produce(self, topic: str, message: dict, max_retries: int = 3):
         """Produce a message to the specified Kafka topic."""
-        try:
-            message_bytes = json.dumps(message).encode('utf-8')
-            self.__producer.produce(topic, value=message_bytes, callback=self.delivery_report)
-            self.__producer.poll(0)
-        except Exception as e:
-            logger.error(f"Produce error: {e}")
+        retries = 0
+        while retries < max_retries:
+            try:
+                message_bytes = json.dumps(message).encode('utf-8')
+                self.__producer.produce(topic, value=message_bytes, callback=self.delivery_report)
+                self.__producer.poll(0)
+                return
+            except BufferError as e:
+                logger.warning(f"Local producer queue is full ({len(self.__producer)} messages awaiting delivery): {e}")
+                self.__producer.poll(1)
+                retries += 1
+            except Exception as e:
+                logger.error(f"Produce error: {e}")
+                break
 
     def flush(self):
+        """Flush the producer to ensure all messages are sent."""
         self.__producer.flush()
