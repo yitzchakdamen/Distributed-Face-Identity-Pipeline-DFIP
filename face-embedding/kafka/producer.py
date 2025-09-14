cd176598f21acf9f4b882e53f6e2b352390801aa
ootb <battles-cyclic-87@icloud.com>
2025-09-14 12:21:05 +0300

    Add KafkaProducer class for producing messages to Kafka

---
+++ face-embedding/kafka/producer.py
@@ -0,0 +1,26 @@
+from confluent_kafka import Producer
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+class KafkaProducer:
+    def __init__(self, config: dict):
+        self.__producer = Producer(config)
+
+    def delivery_report(self, err, msg):
+        if err is not None:
+            logger.error(f"Message delivery failed: {err}")
+        else:
+            logger.info(f"Message delivered to {msg.topic()} [{msg.partition()}]")
+
+    def produce(self, topic: str, message: dict):
+        try:
+            message_bytes = json.dumps(message).encode('utf-8')
+            self.__producer.produce(topic, value=message_bytes, callback=self.delivery_report)
+            self.__producer.poll(0)
+        except Exception as e:
+            logger.error(f"Produce error: {e}")
+
+    def flush(self):
+        self.__producer.flush()
