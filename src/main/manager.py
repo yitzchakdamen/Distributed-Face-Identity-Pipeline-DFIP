0e24035369056c6ef8921aba2d0ba7b885f39843
menachem yarhi <nbjo460@gmail.com>
2025-09-15 10:34:26 +0300

    finish dal

--- src/main/manager.py
+++ src/main/manager.py
@@ -1,5 +1,5 @@
 from src.dal.elastic_dal import ElasticSearchDal
-from src.exceptions.exception import NoIdentifiedPerson, NoAddedVector
+from src.exceptions.exception import NoIdentifiedPerson, NoAddedVector, NoSearchResult
 from src.utils.logger import Logger
 import hashlib
 import json
@@ -10,19 +10,24 @@ class Manager:
         self._es = ElasticSearchDal()
         self.logger = Logger().get_logger()
 
-    def add_vector(self, _id, _vector):
-        try:
-            self._es.add_vector(_vector=_vector, _person_id= _id)
-        except NoAddedVector as e:
-            self.logger.warning(e)
-
     def search_vector(self, _vector):
         try:
             self._es.search_vector(_vector)
         except NoIdentifiedPerson() as e:
             self.logger.info(e)
-            _id = self._generate_id_by_vector(_vector)
-            self.add_vector(_id, _vector)
+            self._add_person_without_id(_vector)
+        except NoSearchResult as e:
+            self.logger.warning(e)
+
+    def _add_vector(self, _id, _vector):
+        try:
+            self._es.add_vector(_vector=_vector, _person_id= _id)
+        except NoAddedVector as e:
+            self.logger.warning(e)
+
+    def _add_person_without_id(self, _vector):
+        _id = self._generate_id_by_vector(_vector)
+        return self._add_vector(_id, _vector)
 
     @staticmethod
     def _generate_id_by_vector(_vector : list) -> str:
