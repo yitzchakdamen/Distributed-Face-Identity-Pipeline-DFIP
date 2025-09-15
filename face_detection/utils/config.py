e8af0491e738e978f78bce34c2c75e224c6e3a96
Eclipse <Eclips77@users.noreply.github.com>
2025-09-15 14:42:22 +0300

    Restore .DOCKERFILE with complete Docker configuration for the application

--- face_detection/utils/config.py
+++ face_detection/utils/config.py
@@ -5,7 +5,7 @@ LOGGER_NAME : str = os.getenv("LOGGER_NAME","logger")
 # MongoDB connection details
 MONGO_URI = os.getenv("MONGO_URI","localhost:27017")
 MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME","face_identity")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME","photo_storage")
+COLLECTION_NAME = os.getenv("COLLECTION_NAME","Photo_storage")
 # Kafka connection details
 KAFKA_BOOTSTRAP = os.getenv("KAFKA_BOOTSTRAP","localhost:9092")
 KAFKA_TOPIC = os.getenv("KAFKA_TOPIC","image_uploaded")
