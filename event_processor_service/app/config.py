461dc9ed6a7d6d8aa0e1259da78c508246ecb265
shlomo <shlomo2096@gmail.com>
2025-09-15 11:41:56 +0300

    Update MongoDB configuration to use consistent database and collection names

--- event_processor_service/app/config.py
+++ event_processor_service/app/config.py
@@ -5,7 +5,7 @@ KAFKA_TOPIC = os.getenv('KAFKA_TOPIC',"event")
 KAFKA_CONSUMER_GROUP = os.getenv('KAFKA_CONSUMER_GROUP',"event_consumer_group")
 
 MONGO_URI = os.getenv('MONGO_URI',"mongodb://localhost:27017/")
-MONGO_DB = os.getenv('MONGO_DB',"event_db")
-MONGO_COLLECTION = os.getenv('MONGO_COLLECTION',"event_collection")
+MONGO_DB = os.getenv('MONGO_DB',"face_identity")
+MONGO_COLLECTION = os.getenv('MONGO_COLLECTION',"Event")
 
 LOGGER_NAME = os.getenv('LOGGER_NAME',"event_processor_service_logger")
