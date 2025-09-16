51f3b8793a12b40192441d67e112ea2a4aaf3704
Ariel Tanami <arieltanami122@gmail.com>
2025-09-16 17:05:06 +0300

    feat: remove unused React import from App component

--- fs-dashboard/client/src/App.tsx
+++ fs-dashboard/client/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import ApplicationLayout from "./components/application-layout/ApplicationLayout";
